58e636ef3bdd5de3468f104d7c5b0192f962954e
cybilemanuela-code <cybilemanuela@gmail.com>
2026-05-21 11:36:21 -0500

    nouveau

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {  getAuth} from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -7,15 +8,16 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyA0545VMkSf0jC97YSdjG9A2_cChV3kmRQ",
+  authDomain: "unibot-56f52.firebaseapp.com",
+  projectId: "unibot-56f52",
+  storageBucket: "unibot-56f52.firebasestorage.app",
+  messagingSenderId: "425060661350",
+  appId: "1:425060661350:web:b8901b1058198c13c215a7",
+  measurementId: "G-R0WKFE9RZ4"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 const analytics = getAnalytics(app);
